ec2194084003608212408f150526a393874206cf
Dariush Forouher <dariush@forouher.de>
2012-06-10 23:10:24 +0200

    more armel tests

---
+++ userspace/hello_world.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <fcntl.h>
+#include <assert.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+
+   printf("Hello World!\n");
+
+}
+
